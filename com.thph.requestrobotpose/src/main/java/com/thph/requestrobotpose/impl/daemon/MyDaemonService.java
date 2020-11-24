f81c4a8f1f6d4ae46b185cf90af2c92eeb98c5a5
thph <thphr16@student.sdu.dk>
2020-11-24 02:26:57 -0800

    daemonservice

---
+++ com.thph.requestrobotpose/src/main/java/com/thph/requestrobotpose/impl/daemon/MyDaemonService.java
@@ -0,0 +1,35 @@
+package com.thph.requestrobotpose.impl.daemon;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.ur.urcap.api.contribution.DaemonContribution;
+import com.ur.urcap.api.contribution.DaemonService;
+
+public class MyDaemonService implements DaemonService {
+
+	private DaemonContribution daemonContribution;
+
+	@Override
+	public void init(DaemonContribution daemon) {
+		this.daemonContribution = daemonContribution;
+		try {
+			daemonContribution.installResource(new URL("file:com/ur/urcap/sample/XMLRPCmath/impl/daemon/"));
+		} catch (MalformedURLException e) {
+		}
+	}
+
+	@Override
+	public URL getExecutable() {
+		try {
+			return new URL("file:com/ur/urcap/sample/XMLRPCmath/impl/daemon/mathserver.py");
+		} catch (MalformedURLException e) {
+			return null;
+		}
+	}
+
+	public DaemonContribution getDaemon() {
+		return daemonContribution;
+	}
+
+}
