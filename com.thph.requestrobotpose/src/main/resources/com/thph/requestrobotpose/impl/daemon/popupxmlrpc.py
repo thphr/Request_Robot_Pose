6cdb5cc4390900d35fb24fd07408691dc1e31d3c
TP <thphr16@student.sdu.dk>
2020-12-22 15:08:42 +0100

    update python file

--- com.thph.requestrobotpose/src/main/resources/com/thph/requestrobotpose/impl/daemon/popupxmlrpc.py
+++ com.thph.requestrobotpose/src/main/resources/com/thph/requestrobotpose/impl/daemon/popupxmlrpc.py
@@ -1,17 +1,25 @@
 import xmlrpclib
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 
+isShowing = False
+
 def showpopup():
-    return "Show popup"
+    isShowing = True
+    return isShowing
+
+def isEnabled():
+    return isShowing
 
 def cancelpopup():
-    return "Cancel popup"
+    isShowing = False
+    return isShowing
 
 server = SimpleXMLRPCServer(("", 40405), allow_none=True)
 
 print "Listening on port 40405..."
 
 server.register_function(showpopup,"showpopup")
-server.register_function(cancelPopup,"cancelpopup")
+server.register_function(cancelpopup,"cancelpopup")
+server.register_function(isEnabled,"isEnabled")
 
 server.serve_forever()
