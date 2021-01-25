06cbabc41d8a5b3fc86e41006a17963e434e9d2a
TP <thphr16@student.sdu.dk>
2021-01-25 13:16:47 +0100

    added buttonhandler to python file

--- com.thph.requestrobotpose/src/main/resources/t_daemon/requestpose_xmlrpc.py
+++ com.thph.requestrobotpose/src/main/resources/t_daemon/requestpose_xmlrpc.py
@@ -11,6 +11,55 @@ from SocketServer import ThreadingMixIn
 isShowing = False
 LOCALHOST = "127.0.0.1"
 
+zNegative = False
+zPositive = False
+
+yNegative = False
+yPositive = False
+
+xNegative = False
+xPositive = False
+
+def setDirectionEnabled(toolspeed,enabled):
+  if toolspeed == "zNegative":
+    global zNegative
+    zNegative = True
+  if toolspeed == "zPositive":
+    global zPositive
+    zPositive = True
+  if toolspeed == "yNegative":
+    global yNegative
+    yNegative = True
+  if toolspeed == "yPositive":
+    global yPositive
+    yPositive = True
+  if toolspeed == "xNegative":
+    global xNegative
+    xNegative = True
+  if toolspeed == "xPositive":
+    global xPositive
+    xPositive = True
+
+def getDirectionEnabled(toolspeed):
+  if toolspeed == "zNegative":
+    global zNegative
+    return zNegative
+  if toolspeed == "zPositive":
+    global zPositive
+    return zPositive
+  if toolspeed == "yNegative":
+    global yNegative
+    return yNegative
+  if toolspeed == "yPositive":
+    global yPositive
+    return yPositive
+  if toolspeed == "xNegative":
+    global xNegative
+    return xNegative
+  if toolspeed == "xPositive":
+    global xPositive
+    return xPositive
+
 def showpopup():
   global isShowing
   isShowing = True
@@ -26,6 +75,7 @@ def cancelpopup():
   return isShowing
 
 
+
 class MultithreadedSimpleXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
     pass
 
@@ -38,6 +88,9 @@ print "Listening on port 40405..."
 server.register_function(showpopup,"showpopup")
 server.register_function(cancelpopup,"cancelpopup")
 server.register_function(isEnabled,"isEnabled")
+server.register_function(setDirectionEnabled,"setDirectionEnabled")
+server.register_function(getDirectionEnabled,"getDirectionEnabled")
+
 
 server.serve_forever()
 
